a76e37620b3074aab8806dfda30c2f086f5ce4a7
sara-likaount <saralikaunt@gmail.com>
2022-05-11 23:49:40 +0300

    n

---
+++ main.js
@@ -0,0 +1,46 @@
+// question 1:
+
+// function printhw (){
+//    for (var i= 0; i < 4; i++ ) {
+//     console.log ("hellow world");
+//     }
+// }
+// printhw();
+
+
+// // question 2:
+// function returnMax(a,b){
+//     if (a>b){
+//         return a;
+//     }
+//     return b;
+// }
+
+// console.log(returnMax(5,4));
+
+// question 3:
+//     function returnSum(num1,num2){
+//         sum= num1+num2
+//         return sum;
+//     }
+
+// console.log(returnSum(5,7));
+
+// // // question 4:
+
+
+// // question 5:
+var a;
+var b;
+
+function switchVar(a,b){
+    a=b
+    b=a
+}
+console.log(switchVar(5,8)) ;
+
+
+// // question 2:
+// // question 2:
+
+

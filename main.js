a062faf3938861f3deaf2e8853060385dffa8abc
sara-likaount <saralikaunt@gmail.com>
2022-05-24 16:02:51 +0300

    m

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
-// question 1:
+// functions:
+// question 59:
 
 // function printhw (){
 //    for (var i= 0; i < 4; i++ ) {
@@ -8,7 +9,7 @@
 // printhw();
 
 
-// // question 2:
+// // question 61:
 // function returnMax(a,b){
 //     if (a>b){
 //         return a;
@@ -18,7 +19,7 @@
 
 // console.log(returnMax(5,4));
 
-// question 3:
+// question 62:
 //     function returnSum(num1,num2){
 //         sum= num1+num2
 //         return sum;
@@ -26,14 +27,65 @@
 
 // console.log(returnSum(5,7));
 
-// // // question 4:
+// // // question 63:
+
+// function calculatesum(num){// 4
+// var sumcount= 0;
+// for(var i=1; i<=num; i++){
+//         sumcount+= i;
+//         console.log(sumcount);  
+// }
+// return sumcount;
+// }
+
+// console.log(calculatesum(4));
 
 
-// // question 5:
 
+// // question 64:
+// var first=5;
+// var second= 4;
+// function twovalus(){
+//         first= second;
+//         second= 5;
+//         console.log(first,second);
+// }
+// twovalus();
 
+////65:
+// function getholenum(starnum){
+//         for(i=0;i<starnum;i++){
+//                 for(j=0;j<i;j++){
+//                         document.write("*");
+//                 }
+//                 document.write("<br>");   
+//         }
+// }
+// getholenum(5);
 
-// // question 2:
-// // question 2:
+//70:
+// function fournums(num1,num2,num3,num4){
+//         result= num1+num2+num3+num4;
+//         return result;
+// }
+
+// console.log(fournums(1,2,3,4)); 
+
+// 71:
+// function be(base ,exponent){
+//         result= base**exponent;
+//         return result;
+// }
+// console.log(be(2,3));
+//72:////////////////////////////////////////////////////////////////////////////
+// function decomposition(number){
+//         y= number%10
+//         a= number%100
+//         Math.floor(a/10)
+
+// }
+// decomposition(134);
+// 73:
+
 
 
